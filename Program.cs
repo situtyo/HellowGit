fdb25fdd0d8e77acf2e51fade215d25cc7c8f5b1
situtyo <situtyo666@gmail.com>
2015-12-03 18:35:04 +0900

    change
    change1

---
+++ Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace 原始的スタック
+{
+    class Program
+    {
+        
+        static int[]stack=new int[10];
+        static int a = 0;
+
+        static void push(int b) 
+        {   
+            stack[a]=b;
+            a++;
+        }
+
+        static int Pop()
+        {
+            a--;
+            return stack[a];
+        }
+
+
+        static void Main(string[] args)
+        {
+            push(1);
+            push(2);
+            Console.WriteLine(Pop());
+            push(3);
+            Console.WriteLine(Pop());
+        }
+    }
+}
